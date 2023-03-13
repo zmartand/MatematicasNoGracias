12f5c00e349070fe724b7160238482dbfc01474d
Zara Martín <zmartand@myuax.es>
2023-03-13 15:31:22 +0100

    Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        Polinomio polinomio = new Polinomio();
+        int termino = 0;
+        int valor = 0;
+        String palabra = "";
+        do {
+            System.out.println("Ingrese un termino");
+            termino = sc.nextInt();
+            System.out.println("Ingrese un valor");
+            valor = sc.nextInt();
+            polinomio.agregarTermino(termino, valor);
+            System.out.println("Ingrese una palabra");
+            palabra = sc.next();
+        } while (!palabra.equals(""));
+        //polinomio.mostrarPolinomioCompleto();
     }
 }
