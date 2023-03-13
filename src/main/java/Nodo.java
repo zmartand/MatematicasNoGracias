7ed37ce385908e9b8aeb64f23165024024ed4242
Zara Martín <zmartand@myuax.es>
2023-03-13 15:18:31 +0100

    Interfaz Nodo

--- src/main/java/Nodo.java
+++ src/main/java/Nodo.java
@@ -1,2 +1,7 @@
-public class Nodo {
+public interface Nodo {
+    public DatoPolinomio getInfo();
+    public void setInfo(DatoPolinomio info);
+    public Nodo getSig();
+    public void setSig(Nodo sig);
+
 }
