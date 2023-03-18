d87693bfe753454037c63005169a09fae76e264c
Zara Martín <zmartand@myuax.es>
2023-03-18 23:04:12 +0100

    Clase Nodo

--- src/main/java/org/example/Nodo.java
+++ src/main/java/org/example/Nodo.java
@@ -1,9 +1,23 @@
 package org.example;
 
-public interface Nodo {
-    public DatoPolinomio getInfo();
-    public void setInfo(DatoPolinomio info);
-    public Nodo getSig();
-    public void setSig(Nodo sig);
+public class Nodo {
+    private DatoPolinomio dato;
+    private Nodo siguiente;
 
+    public Nodo(DatoPolinomio dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }
+
+    public DatoPolinomio obtenerDato() {
+        return this.dato;
+    }
+
+    public Nodo obtenerSiguiente() {
+        return this.siguiente;
+    }
+
+    public void establecerSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
 }
